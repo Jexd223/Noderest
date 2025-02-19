c5b03edaae90ba2a30568567ec196641be150eb2
Jexd223 <s6606021611083@email.com>
2025-02-19 22:01:00 +0700

    Update

--- index.js
+++ index.js
@@ -1,75 +1,95 @@
-require("dotenv").config(); 
 const express = require('express');
-const sqlite3 = require('sqlite3'); //libary database
+const Sequelize = require('sequelize');
 const app = express();
+require("dotenv").config();
 
-const db = new sqlite3.Database(`./Database/Book.sqlite`); // new database
+app.use(express.json());
 
-app.use(express.json()); //send data to json
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    storage: './Database/Books.sqlite'
+});
 
-db.run(`CREATE TABLE IF NOT EXISTS books(
-        id INTEGER PRIMARY KEY,
-        title TEXT,
-        author TEXT
-    )`
-)
+const Book = sequelize.define('book', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    author: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
 
-app.get('/books',(req,res) => {
-    db.all('SELECT * FROM books',(err , rows) =>{
-        if(err){
-            res.status(500).send(err);
-        }else{
-            res.json(rows);
-        }
-    })
-})
+sequelize.sync();
 
-app.get('/books',(req,res) => {
-    db.all('SELECT * FROM books WHERE id = ?',req.params.id,(err , rows) =>{
-        if(err){
-            res.status(500).send(err);
-        }else{
-            if(!row){
-                res.status(404).send('Book not found');
-            }else{
-                res.json(rows);
-            }
-        }
-    })
-})
-
-app.post('/books',(req,res) => {
-    const book =req.body;
-    db.run('INSERT INTO books (title , author) VALUES (?,?)',book.title, book.author,function(err){
-        if(err){
-            res.status(500).send(err);
-        }else{
-            book.id = this.lastID;
-            res.send(book);
-        }
-    })
-})
-
-app.put('/books/:id', (req,res) => {
-    const book = req.body;
-    db.run('UPDATE books SET title = ?, author = ? WHERE id = ?',book.title,book.author,req.params.id,function(err){
-        if (err){
-            res.status(500).send(err);
-        }else{
-            res.send(book)
-        }
-    })
-})
-
-app.delete('/books/:id', (req, res) => {
-    db.run('DELETE FROM books WHERE id = ?', req.params.id, function(err) {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.send({});
-        }
+app.get('/books', (req,res) => {
+    Book.findAll().then(books => {
+        res.json(books);
+    }).catch(err => {
+        res.status(500).send(err);
     });
 });
 
+app.get('/books/:id', (req,res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('book not found');
+
+        } else {
+            res.json(book);
+        }
+    }) .catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.post('/books', (req,res) => {
+    Book.create(req.body).then(book => {
+        res.send(book);
+    }) .catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.put('/books/:id', (req,res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('Book not found');
+        } else {
+            book.update(req.body).then(() => {
+                res.send(book);
+            }).catch(err => {
+                res.status(500).send(err);
+            });
+        }
+    }).catch(err => {
+    res.status(500).send(err);
+    });
+});
+
+app.delete('/books/:id', (req, res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('Book not found');
+        } else {
+            book.destroy().then(() => {
+                res.send({});
+            }).catch(err => {
+                res.status(500).send(err);
+            });
+        }
+    }) .catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
+app.listen(port, () => console.log(`Listen on port http://localhost:${port}...`));
